7a3610d0254058ea387953c79e5b0de5b9be59b1
Maxim Grechkin <grechkin@cs.washington.edu>
2017-02-08 10:30:07 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+A tool for monitoring release of GEO Series after publication
---
+++ rawdata/download.sh
@@ -0,0 +1,3 @@
+aria2c -c -i urls.txt
+wget https://gbnci-abcc.ncifcrf.gov/geo/GEOmetadb.sqlite.gz
+gunzip GEOmetadb.sqlite.gz
